21109851ca9b9ebc7da1675e29b732cc6c5924f2
David <davidopotowsky14@gmail.com>
2024-10-15 19:55:40 -0500

    completed track.js

--- api/tracks.js
+++ api/tracks.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
+module.exports = router;
 
 const prisma = require("../prisma");
 
 router.get("/", async (req, res, next) => {
-    next()
+    try {
+        const tracks = await prisma.track.findMany();
+        res.json(tracks);
+    } catch (e) {
+        next(e);
+    }
     });
 
-module.exports = router;
+router.get("/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const track = await prisma.track.findUniqueOrThrow({ where: { id: +id } });
+        res.json(track);
+    } catch (e) {
+        next(e);
+    }
+    });
+
+
